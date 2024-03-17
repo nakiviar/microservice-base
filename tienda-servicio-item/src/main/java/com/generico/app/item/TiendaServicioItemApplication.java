901d6dbcd206b0a4a11f8bec4bdf05803acd4b88
nakiviar <nakidevi@hotmail.com>
2024-03-17 17:29:51 -0500

    UDMLB18 Actualizando version java 11

--- tienda-servicio-item/src/main/java/com/generico/app/item/TiendaServicioItemApplication.java
+++ tienda-servicio-item/src/main/java/com/generico/app/item/TiendaServicioItemApplication.java
@@ -3,9 +3,11 @@ package com.generico.app.item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
+@RibbonClient(name = "service-products")
 @EnableFeignClients
 @SpringBootApplication
 public class TiendaServicioItemApplication {
