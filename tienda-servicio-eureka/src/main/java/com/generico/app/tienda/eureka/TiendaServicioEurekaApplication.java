c70931a47a019bdd912748dc57028a34f29eaeab
nakiviar <nakidevi@hotmail.com>
2024-03-18 21:49:32 -0500

    UDESRM22 init project eureka server

---
+++ tienda-servicio-eureka/src/main/java/com/generico/app/tienda/eureka/TiendaServicioEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.generico.app.tienda.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaServicioEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaServicioEurekaApplication.class, args);
+    }
+
+}
